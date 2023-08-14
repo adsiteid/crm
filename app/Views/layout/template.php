68dd00285ca090bba54a2dc422368e66d6b9e985
aaonedollar <agitagustian@gmail.com>
2023-08-14 21:40:32 +0700

    bootstrap 5 header

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>CRM ADSITE.ID</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="<?= base_url() ?>vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- plugins:css -->
     <link rel="stylesheet" href="<?= base_url() ?>vendor/adsite/skydash/vendors/feather/feather.css">
