e742402ad94ae0d52e7b0edb248f020476d90717
aaonedollar <agitagustian@gmail.com>
2023-08-22 06:54:47 +0700

    edit update leads

--- app/Views/report/report_sales.php
+++ app/Views/report/report_sales.php
@@ -200,7 +200,7 @@
                                     </td>
 
                                     <td>
-                                        <?= $newleads->getNumRows();
+                                        <?= $all->getNumRows();
                                         ?>
                                     </td>
 
