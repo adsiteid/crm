dc1a17490aa862670e3fc59d2219b6d5323fe169
aaonedollar <agitagustian@gmail.com>
2023-10-28 00:27:02 +0700

    edit

--- app/Views/report/report_sales_range.php
+++ app/Views/report/report_sales_range.php
@@ -459,6 +459,22 @@
     <!-- End of Pagination -->
 
 
-
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 2.5,
+            // slidesPerView: 2.5,
+            spaceBetween: 6,
+            // spaceBetween: 10,
+            freeMode: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    </script>
 
     <?php $this->endSection(); ?>
